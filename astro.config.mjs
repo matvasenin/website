870b48331bba4017d26f483d682f1ce60f50ed74
matvasenin <mat.vasenin@ya.ru>
2026-07-26 17:44:19 +0300

    feat(repo): add support for the sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import pluginIcons from "unplugin-icons/vite";
+import pluginSitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://matvasenin.ru",
+  integrations: [
+    pluginSitemap()
+  ],
   vite: {
     plugins: [
       pluginIcons({
